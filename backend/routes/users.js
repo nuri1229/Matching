4938a3457663814eb04f89b576a3c3dbb398d1b7
choi <yms3684@naver.com>
2018-10-08 17:34:51 +0900

    res.send(users) 주석처리함 vue에서 response.data 받을수있는지 확인요망

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,7 +8,51 @@ var objUsers = require('./obj/objUsers.js');
 router.get('/', function(req, res, next) {
   var users = objUsers.users; //JSON ?
   console.log('여기는 Users.js users = ',users);
-  res.send(users);
+  //res.send(users);
 });
+
+router.get('/profile', function(req, res, next) {
+ console.log('여기는 프로필');
+
+});
+
+router.get('/profile/update', function(req, res, next) {
+ 
+
+});
+
+router.get('/profile/terminated', function(req, res, next) {
+ 
+
+});
+
+router.get('/profile/portfolio/list', function(req, res, next) {
+ 
+
+});
+
+
+router.get('/profile/portfolio/view', function(req, res, next) {
+ 
+
+});
+
+router.get('/profile/portfolio/create', function(req, res, next) {
+ 
+
+});
+
+router.get('/profile/portfolio/update', function(req, res, next) {
+ 
+
+});
+
+router.get('/profile/portfolio/delete', function(req, res, next) {
+ 
+
+});
+
+
+
 
 module.exports = router;
