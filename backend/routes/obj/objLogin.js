e95ef3594ae7d886376a5019b356f404294e563f
choi <yms3684@naver.com>
2018-10-09 14:47:15 +0900

    디버깅용 console추가

--- backend/routes/obj/objLogin.js
+++ backend/routes/obj/objLogin.js
@@ -1,5 +1,5 @@
 var db = require('../db/db.js');
-
+var sess;
 
 exports.CheckId_Pw = function(req,res,ID,PW){
     var sql = 'select * from tb_user where user_id=? and user_pw=?';
@@ -8,29 +8,26 @@ exports.CheckId_Pw = function(req,res,ID,PW){
         if(err){
             throw err;
         }else{
-            //JSON으로 만들기
-           /*  function JSONfy(user_id,message){
-                this.user_id =user_id;
-                this.message = message;
-            } */
-            var result={};
+ 
+            var result={};//JSON객체
 
             if(data.length == 0){//로그인 실패
+
                 var message='failed';
-               // var result = new JSONfy('',message);
-                
+               
                 result["user_info"]=null;
                 result["message"]=message;
                 console.log(result);
                 res.send(result);
 
             }else{//로그인 성공
-                var sess;
+                console.log('data[0]을 찍어보자!! >>',data[0]);                
                 sess = req.session;
-                sess.user_info = data[0];//세션담기   
-                      
+                console.log('sess을 찍어보자!!>>',sess);
+                sess.user_info=data[0] ;
+                console.log('sess.user_info!! >>',sess.user_info);
 
-                if(data[0].user_id === 'admin'){
+                if(data.user_id === 'admin'){
                     var message='admin';                              
                 }else{
                     var message=`success`;
@@ -39,7 +36,7 @@ exports.CheckId_Pw = function(req,res,ID,PW){
                 result["user_info"]=sess.user_info;
                 result["message"]=message;
 
-                //var result = new JSONfy(sess.user_id,message); 
+               
 
                 console.log(result);
                 res.send(result);
