bc085381f5fd953c6150241e407b3fc480579fe7
choi <yms3684@naver.com>
2018-10-13 13:04:47 +0900

    변경사항에따른 내용변화

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');//추가
-var db = require('./routes/db/db.js');
-
-
-
 
 /* Import Router(S) */
 var IndexRouter = require('./routes/index');
-var UsersRouter = require('./routes/Users');
 var LoginRouter = require('./routes/Login');
 var SignUpRouter = require('./routes/SignUp');
 var MatchingRouter = require('./routes/Matching');
@@ -37,10 +32,9 @@ app.use(session({
   resave: false,
   saveUninitialized: true
  }));
-
+ 
 /* 라우팅동작구문(S) */
 app.use('/', IndexRouter);
-app.use('/api/users', UsersRouter);
 app.use('/api/Login', LoginRouter);
 app.use('/api/SignUp', SignUpRouter);
 app.use('/api/Matching', MatchingRouter);
@@ -48,28 +42,8 @@ app.use('/api/Admin', AdminRouter);
 app.use('/api/Board', BoardRouter);
 app.use('/api/Location', LocationRouter);
 app.use('/api/Genre', GenreRouter);
-
-//TEST용
-app.post('/api/idDuplicateCheck',function(req,res,next){
-  console.log('여기는 아이디중복체크.. ');
-    var ID = req.body.user_id;
-    console.log('ID = ', ID);
-    db.query('select user_id from tb_user where user_id=?',[ID],function(err,data,fields){
-        console.log('data = ',data);  
-        if(err){
-            throw err;
-        }else{
-            var result;
-            if(data.length == 0){ //ID 중복X
-                result = '0';
-            }else{ //ID 중복
-                result = '1';
-            }
-            res.send(result);
-        }
-    });
-});
 /* 라우팅동작구문(E) */
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
