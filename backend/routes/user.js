7119833de18c6a269fcf074a1082fca57ea25b65
choi <yms3684@naver.com>
2018-10-14 03:02:25 +0900

    ++추가사항++ 파일업로드완료하였습니다!

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var objUsers = require('./obj/objUsers.js');
 const fileUpload = require('express-fileupload');
-router.use(fileUpload());
+var db = require('./db/db.js');
 var fs= require('fs');
+
+
+router.use(fileUpload());
 
 //회원정보화면
 router.get('/profile',function(req,res,next){
@@ -38,24 +41,113 @@ router.get('/portfolio/view',function(req,res,next){
 //회원포폴 추가UI
 router.post('/portfolio/create',function(req,res,next){
     console.log('req.body->',req.body,'\n req.files -> ',req.files);
-    //var po_data = req.body.po_data;
-    //console.log('po_data ->',po_data);
-    //console.log('req.body.po_data[\'user_id\'] ->',req.body.po_data['user_id']);//undefined
-    //console.log('req.body.po_data.get(\'user_id\') ->',req.body.po_data.get('user_id')); //문법오류
-    //console.log(Object.keys(req.body.po_data)); //0~14
-    //console.log('JSON.stringfy(req.body.po_data).user_id -> ',JSON.stringify(req.body.po_data).user_id);
-    //console.log('typeof(req.body.po_data) -> ',typeof(po_data));
-    var po_data = req.params.po_data;
-    console.log(po_data);
-    
-   /*  var i = 0;
-    while(i<Object.keys(po_data).length){
-      var value =po_data[i];
-      console.log(`value[${i}] ->`,value[i]);
-      i++;
-    } */
+    var po_data=JSON.parse(req.body.po_data);
+      
 
-    res.send('ddd');
+     //▼공통 변수
+     var d= new Date();
+     var getYear = d.getFullYear() ;
+     var getMonth = d.getMonth()+1;
+     var getDate = d.getDate();
+     var getHours = d.getHours();
+     var getMinutes = d.getMinutes();
+     var getSec = d.getSeconds();
+     var Converted = ''+getYear+getMonth+getDate+getHours+getMinutes+getSec;
+     var AlphabetArr = 
+         ['a','b','c','d','e',
+         'f','g','h','i','j',
+         'k','l','m','n','o',
+         'p','q','r','s','t',
+         'u','v','w','x','y',
+         'z','0','1','2','3','4','5','6','7','8','9'];
+     var po_number = 'PIDX';
+     var cnt=0;
+     while(cnt< 12){
+         var AlphabetRandomKey = Math.floor(Math.random()*AlphabetArr.length); 
+         console.log(AlphabetRandomKey);
+         po_number+=AlphabetArr[AlphabetRandomKey];
+         cnt++;
+     }
+     //▼새변수명
+     var user_id=po_data['user_id'];
+     console.log('user_id->',user_id);
+     var po_view_count=po_data['po_view_count'];
+     var po_apply_count=po_data['po_apply_count'];
+     var po_title=po_data['po_title'];
+     var genre_number = parseInt(po_data['gen_number']);
+     var po_type=po_data['po_type'];
+     var po_desc=po_data['po_desc'];
+    
+     //▼포폴파일변수
+     var uploadedData = req.files.uploadData; //넘겨온 파일 받는부분
+     console.log('post요청받음 \n넘겨온파일녀석 => ',uploadedData);
+     var OrinalFileName= uploadedData.name; //po_file_username
+     var ConvertedFileName= Converted+OrinalFileName; //po_file_name
+     var FilePath = `./data/${user_id}`;//po_file_path
+     console.log('po_file_username ->',OrinalFileName,'\n',
+                 'po_file_name ->',ConvertedFileName,'\n',
+                 'po_file_path ->',FilePath);
+     
+     //▼폴더가 있는지없는지!
+     console.log('Filepath ->',FilePath,'\n 검사들어갑니다!');
+ 
+     if (!fs.existsSync(FilePath)){ //없다면
+         fs.mkdirSync(FilePath);//만든다
+         console.log('경로에 폴더가 없어서 만들었습니다');
+     }else{//있다면
+         console.log('경로에 이미폴더가 만들어져있습니다');
+     }
+ 
+     //▼파일업로드작업
+     uploadedData.mv(`${FilePath}/${ConvertedFileName}`, function(err) {    
+         if (err){
+           res.send('파일업로드실패');
+         }else{ //else시작 
+             console.log('파일업로드완료!');
+             //▼DB작업
+             var sql =`select * from tb_user where user_id='${user_id}'`; 
+             console.log('user_id ->',user_id);
+             db.query(sql,function(err,data,fields){
+                 if(err){
+                      throw err;
+                 }else{
+                     console.log('1차쿼리 성공\n data ->',data);
+                     
+                     var user_nickname = data[0].user_nickname;
+                     console.log('user_nickname ->',user_nickname);
+                     var user_number = data[0].user_number;
+                     console.log('user_number ->',user_number);
+ 
+ 
+                     var sql2= 'insert into tb_portfolio values (?,?,?,?,?,?,?,?,?,?,default,?,default,?,?);';
+                     db.query(sql2,
+                         [po_number,
+                         user_number,
+                         genre_number,
+                         po_type,
+                         ConvertedFileName,
+                         OrinalFileName,
+                         FilePath,
+                         po_title,
+                         po_view_count,
+                         po_apply_count,
+                         user_nickname,
+                         user_nickname,
+                         po_desc],
+                         function(err2,data2,fields){
+                             if(err2){
+                                 throw err2;
+                             }else{
+                                 res.send('DB추가완료!');
+                         }
+                     });
+                 }            
+             });//쿼리끝
+         }//else끝
+     });//업로드 끝
+
+   
+    
 });
 
 
