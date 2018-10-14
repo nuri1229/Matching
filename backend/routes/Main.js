de65d4153c039f66a62782a05a6c2947a5896d6d
choi <yms3684@naver.com>
2018-10-14 20:12:39 +0900

    ++수정사항++ 혹시모를 언디파인드처리

--- backend/routes/Main.js
+++ backend/routes/Main.js
@@ -11,38 +11,43 @@ console.log('req.body.gen_number ->',gen_number);
 var FilterCondtions = {};
 
 //parameter filtering 
-if(gen_number == 0){
-    console.log('전체');
-}else if(gen_number ==1){
-    console.log('일상');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==2){
-    console.log('로맨스');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==3){
-    console.log('액션');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==4){
-    console.log('개그');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==5){
-    console.log('판타지');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==6){
-    console.log('시대극');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==7){
-    console.log('학원');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==8){
-    console.log('멜로');
-    FilterCondtions["g.gen_number"]=gen_number;
-}else if(gen_number ==9){
-    console.log('스포츠');
-    FilterCondtions["g.gen_number"]=gen_number;
+if(gen_number == undefined){
+    console.log('undefined 지만 전체로검색');
 }else{
-
+    if(gen_number == 0){
+        console.log('전체');
+    }else if(gen_number ==1){
+        console.log('일상');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==2){
+        console.log('로맨스');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==3){
+        console.log('액션');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==4){
+        console.log('개그');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==5){
+        console.log('판타지');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==6){
+        console.log('시대극');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==7){
+        console.log('학원');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==8){
+        console.log('멜로');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else if(gen_number ==9){
+        console.log('스포츠');
+        FilterCondtions["g.gen_number"]=gen_number;
+    }else{
+    
+    }
 }
+
 
 objMatching.getAllportfolios(req,res,FilterCondtions); //암것도없으면 {}로넘어감
 
