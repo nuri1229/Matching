84c509fdb8f92455a403cdace50827a078a3077e
choi <yms3684@naver.com>
2018-10-17 16:56:24 +0900

    포폴셀렉트,포폴업로드 문제해결완료했어요.

--- backend/routes/Matching.js
+++ backend/routes/Matching.js
@@ -56,50 +56,23 @@ router.post('/list', function(req, res, next) {
           FilterCondtions["u.user_age"]=agesQuery;
       }         
     }
-    objMatching.getAllportfolios(req,res,FilterCondtions); //암것도없으면 {}로넘어감
+    //▼ 최소 옵션
+    FilterCondtions["p.po_open_yn"]='Y';//공개글불러오기
+
+    objMatching.getAllportfolios(req,res,FilterCondtions); //암것도없으면 {'p.po_open_yn':'Y'}로넘어감
 
 });
 
 //결과물상세보기
-router.get('/view/:pk', function(req, res, next) {
-  /*만약 Url창에 /view/:pk 를 직접치고들어올경우
-  파라미터값을 정해주는 IF문 작성필요*/
+router.post('/view', function(req, res, next) {
   console.log('작업물상세보기');
   res.send('SqlObject');
 });
 
-//신청UI페이지
-router.get('/apply', function(req, res, next) {
-  console.log('신청UI');
-  res.send('SqlObject');
-});
 
-//신청Process
 router.post('/apply',function(req,res,nex){
   console.log('신청 Process');
   res.send('.....');
 });
-
-
-
-//수신된신청목록
-router.get('/reply/list', function(req, res, next) {
-  console.log('수신된신청목록');
-  res.send('SqlObject');
-});
-
-
-//수신된 신청상세보기
-router.get('/reply/view',function(req,res,nex){
-  console.log('신청 Process');
-  res.send('.....');
-});
-
-//답장전송
-router.post('/reply/process',function(req,res,nex){
-  console.log('수락/거절 Process');
-  res.send('.....');
-});
-
 
   module.exports = router;
