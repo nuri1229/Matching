a2c1fd3bf2bb6eceec021c3577158cfea48660f4
choi <yms3684@naver.com>
2018-11-07 14:11:45 +0900

    ++추가사항++ 더미데이터(로그인+포폴) NOW로 찍기때문에 테스트시 날짜가 바뀌면 매번 인위적으로 생성해줘야함

--- matching_sql/분석테이블더미데이터.sql
+++ matching_sql/분석테이블더미데이터.sql
@@ -9,3 +9,25 @@ insert into tb_analyst (search_gen_number,created) values(8,default);
 /*로그인+장르검색*/
 insert into tb_analyst (login_user_number,search_gen_number,created) values('aaaaaaaa00000002',1,default);
 
+/*DUMMY START: 로그인+포폴구경*/
+insert into tb_analyst (login_user_number,search_gen_number,visited_po_number) 
+VALUES ('aaaaaaaa00000002',6,'PIDX8xih51y98c7p');
+
+insert into tb_analyst (login_user_number,search_gen_number,visited_po_number) 
+VALUES ('aaaaaaaa00000002',9,'PIDXu40gipra42zy');
+
+insert into tb_analyst (login_user_number,search_gen_number,visited_po_number) 
+VALUES ('aaaaaaaa00000003',6,'PIDX8xih51y98c7p');
+
+insert into tb_analyst (login_user_number,search_gen_number,visited_po_number) 
+VALUES ('aaaaaaaa00000003',9,'PIDXu40gipra42zy');
+
+insert into tb_analyst (login_user_number,search_gen_number,visited_po_number) 
+VALUES ('aaaaaaaa00000003',1,'PIDXoaglk51xr7ab');
+
+insert into tb_analyst (login_user_number,search_gen_number,visited_po_number) 
+VALUES ('aaaaaaaa00000003',1,'PIDXvx3nnvy6m93c');
+
+insert into tb_analyst (login_user_number,search_gen_number,visited_po_number) 
+VALUES ('aaaaaaaa00000003',1,'PIDXvx3nnvy6m93c');
+/*DUMMY END  */
